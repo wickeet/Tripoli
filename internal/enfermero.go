c1c7b54550b5335e5ad47a942108b057f325b809
Hossam El Amraoui <hossam@correo.ugr.es>
2024-10-23 12:47:15 +0200

    closes wickeet#11, dentro del módulo de enfermero se ha decidido añadir dos structs para representar un periodo de tiempo y un periodo de fechas. Luego, dentro del stuct de enfermero, se ha decicido añadir como preferencias un vector de turnos preferidos, donde se puede incluir periodos de tiempo. También se ha creado un struct de disponibilidad donde se indica el horario de cada enfermero más las fechas donde no se encuentra disponible, añadiendo después esta disponibilidad al stuct de Enfermero. Se ha hecho así porque opino que es la manera más clara y versátil de hacerlo, teniendo en cuenta, en principio, cualquier posibilidad

--- internal/enfermero.go
+++ internal/enfermero.go
@@ -1,8 +1,28 @@
 package internal
 
+import "time"
+
+type PeriodoTiempo struct {
+	DiaSemana  time.Weekday
+	HoraInicio time.Time
+	HoraFin    time.Time
+}
+
+type PeriodoFecha struct {
+	FechaInicio time.Time
+	FechaFin    time.Time
+}
+
+type Disponibilidad struct {
+	HorariosDisponibles    []PeriodoTiempo
+	FechasIndisponibilidad []PeriodoFecha
+}
+
 type Enfermero struct {
-	ID        int
-	Nombre    string
-	Apellidos string
-	Rol       string
+	ID               int
+	Nombre           string
+	Apellidos        string
+	Rol              string
+	TurnosPreferidos []PeriodoTiempo
+	Disponibilidad   Disponibilidad
 }
