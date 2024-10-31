deeec38fcdad481903a58fd0a5115ce8c1b3f0dd
Hossam El Amraoui <hossam@correo.ugr.es>
2024-10-31 13:10:10 +0100

    En relación a wickeet#11 y siguiendo la misma lógica que en https://github.com/wickeet/Tripoli/pull/7/commits/33859714f847c4380b1572b546bf317a3355b93a, las estructuras de datos mínimas necesarias para representar esto ya están hechas, por lo que no tiene mucho sentido añadir unos nuevos tipos basados en esto, dejándose así simplemente el strcut de enfermero y turno, junto a las normas del convenio. closes wickeet#11

--- internal/enfermero.go
+++ internal/enfermero.go
@@ -1,13 +1,7 @@
 package internal
 
-import "time"
-
 type Enfermero struct {
 	nombre    string
 	apellidos string
 	rol       string
 }
-
-type TurnosPreferidos map[*Enfermero][]Turno
-
-type Disponibilidad map[*Enfermero][]time.Weekday
