82cc65b5b5c96e97ea9183806b3823d47c16c62d
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-06 14:31:05 +0100

    En relación a wickeet#13 y continuando lo dicho en https://github.com/wickeet/Tripoli/pull/7/commits/4189d54ae5f87c4a3d4b8cb8756bfad094b8dac1, además de siguiendo la misma lógica que en https://github.com/wickeet/Tripoli/pull/7/commits/65a5cc21fb6c809c0dd66f6b1bcf7fc226c048af, se ha decidido introducir un constructor para el struct HoraMinuto para evitar valores incorrectos, como puede ser un 27:98. Además, se ha pensado en limitar las horas de inicio de turno a horas relativamente lógicas, pudiendo empezar a en punto, y cuarto, y media, o menos cuarto, en vez de empezar, por ejemplo, a las 15:23, cosa que no tiene demasiado sentido. Además se ha introducido manejo de errores. closes wickeet#13

--- internal/turno.go
+++ internal/turno.go
@@ -1,10 +1,34 @@
 package internal
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type HoraMinuto struct {
 	hora   int
 	minuto int
+}
+
+func NewHoraMinuto(hora, minuto int) (*HoraMinuto, error) {
+	if !esHoraValida(hora, minuto) {
+		return nil, errors.New("hora inválida")
+	}
+	return &HoraMinuto{hora: hora, minuto: minuto}, nil
+}
+
+func esHoraValida(hora, minuto int) bool {
+	if hora < 0 || hora > 23 {
+		return false
+	}
+
+	minutosValidos := []int{0, 15, 30, 45}
+	for _, m := range minutosValidos {
+		if minuto == m {
+			return true
+		}
+	}
+	return false
 }
 
 type Turno struct {
