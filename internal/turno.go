4189d54ae5f87c4a3d4b8cb8756bfad094b8dac1
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-06 14:24:51 +0100

    En relación a wickeet#13 y https://github.com/wickeet/Tripoli/pull/7\#discussion_r1826521439, además de https://github.com/wickeet/Tripoli/pull/7/commits/c8ac4d13b145d3fec24a204acf28480cb7338237 y siguiendo la misma lógica que en https://github.com/wickeet/Tripoli/pull/7/commits/295ab91e3df4aa9664796ead86e3768babe2f8a0, se ha metido un struct llamado HoraMinuto el cual solamente incluirá la hora y minuto a la que empieza el turno, evitando así poner segundos (no tiene mucho sentido) o poner días o años (cosa permitida por time.Time). closes wickeet#13

--- internal/turno.go
+++ internal/turno.go
@@ -2,8 +2,13 @@ package internal
 
 import "time"
 
+type HoraMinuto struct {
+	hora   int
+	minuto int
+}
+
 type Turno struct {
 	diaSemana time.Weekday
-	inicio    time.Time
+	inicio    HoraMinuto
 	duracion  time.Duration
 }
